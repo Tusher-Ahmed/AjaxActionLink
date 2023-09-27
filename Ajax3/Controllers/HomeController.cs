b611829f16a09c0c851bd860c1259e75d483d4c4
Tusher Ahmed <ahmedshad1997@gmail.com>
2023-09-27 15:06:30 +0600

    Add project files.

---
+++ Ajax3/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Ajax3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ajax3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly EmployeeDbEntities _db;
+        public HomeController()
+        {
+                _db = new EmployeeDbEntities();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AllEmployee()
+        {
+            var data = _db.Employees.ToList();
+            return PartialView("_EmployeePartial",data);
+        }
+        public ActionResult Top3Employee()
+        {
+            var data = _db.Employees.OrderByDescending(u=>u.Age).Take(3).ToList();
+            return PartialView("_EmployeePartial",data);
+        }
+        public ActionResult Bottom3Employee()
+        {
+            var data = _db.Employees.OrderBy(u => u.Age).Take(3).ToList();
+            return PartialView("_EmployeePartial", data);
+        }
+
+    }
+    
+}
---
+++ Ajax3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ajax3.MvcApplication" Language="C#" %>
